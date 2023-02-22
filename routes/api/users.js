f9c09a4b8ceb87c9b28a1785e66507f7c6f28c8d
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-02-22 14:16:41 +0200

    added verification

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,19 @@
 const { Router } = require('express');
 
-const { updateSubscriptionJoiSchema } = require('../../models');
+const { updateSubscriptionJoiSchema, verifyEmailJoiSchema } = require('../../models');
 const { controllerWrapper, auth, validation, upload } = require('../../middlewares');
 
 const {
-  usersControllers: { getCurrent, updateSubscription, updateAvatar },
+  usersControllers: { getCurrent, updateSubscription, updateAvatar, verifyEmail, resendVerify },
 } = require('../../controllers');
 
 const router = Router();
 
+router.post('/verify', validation(verifyEmailJoiSchema), controllerWrapper(resendVerify));
+
 router.get('/current', auth, controllerWrapper(getCurrent));
+
+router.get('/verify/:verificationToken', controllerWrapper(verifyEmail));
 
 router.patch(
   '/',
